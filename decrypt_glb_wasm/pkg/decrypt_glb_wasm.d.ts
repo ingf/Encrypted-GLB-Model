5e5c39d52e5ba95f8a9bfed8b0adf2ff47fd0c55
YinFeng <yinfeng@mgtv.com>
2024-07-18 18:14:48 +0800

    feat: add target pkg

---
+++ decrypt_glb_wasm/pkg/decrypt_glb_wasm.d.ts
@@ -0,0 +1,39 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Uint8Array} encrypted_data
+* @param {Uint8Array} key
+* @param {Uint8Array} iv
+* @returns {Uint8Array}
+*/
+export function decrypt_glb(encrypted_data: Uint8Array, key: Uint8Array, iv: Uint8Array): Uint8Array;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly decrypt_glb: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
